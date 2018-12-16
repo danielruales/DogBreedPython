701e6dab081092e0a928dd10e013b83acad9cb44
Daniel Ruales <danielruales@gmail.com>
2018-12-15 22:51:16 -0500

    GG BOIZ

--- app.py
+++ app.py
@@ -3,16 +3,38 @@ from data import displayData
 from randomizer import Randomize
 from random import randint
 import requests
+import os
 
 app = Flask(__name__)
 count_list = []
 
+UPLOAD_FOLDER = "/Users/danielruales/Documents/aaaProjects/test/"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
 def index():
-	global count_list
-	if request.method == 'GET':
-		count_list, rand_int = Randomize(count_list)
-		return render_template('unity_page.html', unity_data = displayData(rand_int))
+	print(UPLOAD_FOLDER)
+	print(app.config['UPLOAD_FOLDER'])
+	#print(request.files['image'])
+	#print(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+	return render_template('index.html')
+	#global count_list
+	#if request.method == 'GET':
+		#count_list, rand_int = Randomize(count_list)
+		#return render_template('unity_page.html', unity_data = displayData(rand_int))
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+	#print(UPLOAD_FOLDER)
+	#print(app.config['UPLOAD_FOLDER'])
+	file = request.files['image']
+	print(file)
+	f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
+	#f = "/Users/danielruales/Documents/aaaProjects/test/" + file
+	file.save(f)
+	return render_template('index.html', gg_data = file)
 
 if __name__ == '__main__':
+	app.debug = True
 	app.run()
